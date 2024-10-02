d786fb97c8ba0d0f98acbdcaf9d255cf63431b05
GustavoPresilli <gustavo.presilli@sptech.school>
2024-10-01 21:28:12 -0300

    Atualizando SCRIPT para EC2

--- captura_bucket.py
+++ captura_bucket.py
@@ -1,3 +1,10 @@
+# 1 º Passo - Trocar as variáveis com o que está escrito nela
+# 2 º Passo - Mandar esse script pra EC2
+# 3 º Passo - Conectar na EC2, e criar um ambiente virtual
+# Dentro desse ambiente virtual de python, tem que dar pip install boto3
+# Rodar o SCRIPT de PYTHON e mandar para o BUCKET
+ 
+
 import psutil
 import time
 from socket import gethostname
@@ -10,22 +17,14 @@ sistemaOperacional = platform.system()
 intervalo = int(input("Digite o intervalo do monitoramento: \n")) 
 qtdCapturas = int(input("Digite quantas capturas deseja fazer: "))
 
-# Nome do bucket e caminho do arquivo
-nomeBucket = 'bucket-raw-lab'
-caminhoArquivo = '/home/ubuntu/script-python/teste.csv'
-nomeArquivoBucket = 'dadosMaquina-no-s3.csv'  # Nome que o arquivo terá no S3
-
-# Conectar ao serviço S3
 s3_client = boto3.client('s3')
 
-# Criar o arquivo CSV e escrever o cabeçalho uma vez
-with open('Seu CAMINHO', 'w', newline='') as csvfile:
+with open('SEU CAMINHO NA EC2', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile, delimiter=',')
     csv_writer.writerow(['Uso CPU (%)', 'Freq CPU (MHz)', 'Mem Total (GB)', 'Mem Usada (GB)', 'Uso Memória (%)', 'Disco Total (GB)', 'Disco Usado (GB)', 'Uso Disco (%)'])
 
 
 def upload_to_s3(file_name, bucket, object_name=None):
-    # Se não foi especificado o nome do objeto no S3, usar o nome do arquivo
     if object_name is None:
         object_name = file_name
 
@@ -38,10 +37,7 @@ def upload_to_s3(file_name, bucket, object_name=None):
 
     s3_client = session.client('s3')
 
-
-    # Criar uma sessão e um cliente do S3
     try:
-        # Realizar o upload
         s3_client.upload_file(file_name, bucket, object_name)
         print(f"Arquivo '{file_name}' enviado com sucesso para o bucket '{bucket}'!")
     except FileNotFoundError:
@@ -49,9 +45,7 @@ def upload_to_s3(file_name, bucket, object_name=None):
 
 
 
-while True:
-    #Variáveis de captura dos dados
-    
+for i in range(qtdCapturas):
     porcent_cpu = psutil.cpu_percent()
     memoria = psutil.virtual_memory()
     freq_cpu = psutil.cpu_freq().current
@@ -63,8 +57,8 @@ while True:
     
 
     print(""" 
-    DADOS ARMAZENADOS
-          
+    Captura {:.0f}
+             
     (Dados sendo capturados a cada {:.2f}s)
           
     CPU:      
@@ -80,14 +74,14 @@ while True:
     Disco usado: {:f} 
           
     Pressione Ctrl+C para encerrar a captura
-    """.format(intervalo, porcent_cpu, round(freq_cpu),  memoria.total/pow(10, 9), memoria.percent, round(memoria.used/pow(10,9)), disco.total/pow(10, 9), disco.percent, round(disco.used/pow(10,9))))
+    """.format(i, intervalo, porcent_cpu, round(freq_cpu),  memoria.total/pow(10, 9), memoria.percent, round(memoria.used/pow(10,9)), disco.total/pow(10, 9), disco.percent, round(disco.used/pow(10,9))))
     
-    with open('Seu CAMINHO', 'a', newline='') as csvfile:
+    with open('SEU CAMINHO NA EC2', 'a', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=',')
         csv_writer.writerow([porcent_cpu, round(freq_cpu), round(memoria.total/pow(10, 9), 2), round(memoria.used/pow(10, 9), 2), memoria.percent, round(disco.total/pow(10, 9), 2), round(disco.used/pow(10, 9), 2), disco.percent])  
     
-    #Tempo de captura de dados
-    file_name = 'NOME DO AQRQUIVO NO '
+   
+    file_name = 'NOME DO ARQUIVO QUE VAI PRO BUCKET'
     bucket_name = 'bucket-raw-lab'
     upload_to_s3(file_name, bucket_name)
     time.sleep(intervalo)
